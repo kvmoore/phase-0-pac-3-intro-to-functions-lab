746355168a9658e69ecd97ed99cfdb417a83ca72
Kenneth <kvmoore097@gmail.com>
2023-06-06 19:03:44 -0500

    First functions lab

---
+++ index.js
@@ -0,0 +1,59 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+
+  function logShout(string) {
+    console.log(string.toUpperCase());
+  }
+
+  function logWhisper(string) {
+    console.log(string.toLowerCase());
+  }
+
+
+
+  function sayHiToHeadphonedRoommate(string) {
+
+if (string === string.toLowerCase()) {
+
+return ('I can\'t hear you!')
+
+}
+
+if (string === string.toUpperCase()) {
+
+return ('YES INDEED!')
+
+}
+
+if (string === 'Let\'s have dinner together!') {
+
+return ('I would love to!');
+
+}
+
+  }
+
+/*
+
+
+if (age >= 18) {
+  isAdult = true;
+}
+
+console.log(isAdult);
+
+
+
+
+  
+const string = "Hello!";
+
+console.log(string);
+console.log(string.toUpperCase());
+
+console.log(string === string.toUpperCase()); */
